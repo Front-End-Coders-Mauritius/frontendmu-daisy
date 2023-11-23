cb0e93050cb30fa906d3f03c5ed0171562414a20
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-11-23 20:54:42 +0400

    added daisy ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+const daisyui = require("daisyui")
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [daisyui],
 }
